300d9a80f4bbd8bb75bb4eccbeb691059878944a
Jesslynn marietta.B <priscillabritto554@gmail.com>
2026-05-18 19:12:35 +0530

    Initial commit

---
+++ lesson 1 activity 1/python routine.py
@@ -0,0 +1,32 @@
+# Abour my Daily Routine
+
+print("My Daily Routine")
+#step 1: waking up
+
+print("I wake up at 5 AM")
+
+#step 2:exercising and studing
+
+print("I finish up my exercise at 5:301 AM and recall what i studied till 6:30 AM")
+
+#step 3:Going to school
+
+print("I go to school at 8 AM")
+
+#step 4:snack time!!!
+
+print("I used to have delicious snacks after school hours")
+
+#step 5: TV time!
+print("I wacth NETFLIX for some time")
+
+#step 6:studing hours
+print(" I study for 2 hours in the night")
+
+#step 7:dinner and sleep time
+
+print("I used to have my dinner at 8 PM and go to sleep by 10 PM")
+
+#TOTAL STUDING HOURS IN A WEEK
+
+print("total studing hours in a week 2+1=3*7",3*7)
