1c1d5853c0159d3cd7b288b27bc9acaffdb2e4dd
Jesslynn marietta.B <priscillabritto554@gmail.com>
2026-05-20 17:26:26 +0530

    finish lesson 2 activity 1

---
+++ Lessons/Lesson 2/activity 1.py
@@ -0,0 +1,13 @@
+# TASK 1: Print a Sentence
+print("Today we are on lesson 2 python course.")
+# TASK 2: Print a Number
+print(80720)
+# TASK 3: Next Line after print
+print("This is a new line \n\n\n")
+print("I love icecream \n\n\n")
+# TASK 4: Pass multiple arguments for printing
+print("The sum of 5 and 6",5+6,"That's the sum")
+# TASK 5: "end" argument of Print Statement
+print("Happy", end="! ")
+print("Birthday")
+# TASK 6: What happens with/without "end" argument
