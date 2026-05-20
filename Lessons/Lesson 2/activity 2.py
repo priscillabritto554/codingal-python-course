b2322839a72496ccf1dfb34fb91f0ccc9e231167
Jesslynn marietta.B <priscillabritto554@gmail.com>
2026-05-20 17:46:55 +0530

    finished lesson 2 activity 2

---
+++ Lessons/Lesson 2/activity 2.py
@@ -0,0 +1,10 @@
+name = "Jesslynn Marietta.B"
+age = 16
+percentage = 93.5
+
+print(name)
+print(age)
+print(percentage)
+
+hobby = input("What is your favourite hobby?")
+print(" My hobby is",hobby)
