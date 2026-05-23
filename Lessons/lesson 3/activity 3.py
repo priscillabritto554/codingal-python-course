1af479706ddc4a415d695b008bb373c0db825049
Jesslynn marietta.B <priscillabritto554@gmail.com>
2026-05-23 13:02:16 +0530

    finished lesson 3 activity 3

---
+++ Lessons/lesson 3/activity 3.py
@@ -0,0 +1,15 @@
+# 1) Ask the user to enter a word or sentence and store it in `text`.
+# 2) Reverse the string stored in `text` and store the reversed result in `revText`.
+
+# (Reversing means the last character becomes first, and the first becomes last.)
+
+# 3) Replace `text` with the reversed string (set `text` equal to `revText`).
+
+# 4) Print a message saying you are showing the reversed string.
+
+# 5) Print the reversed string stored in `text`.
+
+text = input("Enter a string: ")
+revtext = text[::-1]
+text = revtext
+print("reversed text is",text)
