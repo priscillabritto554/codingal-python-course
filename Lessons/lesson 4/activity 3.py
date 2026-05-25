23ba8c41de460fd305221db42d00bd3793aca94d
Jesslynn marietta.B <priscillabritto554@gmail.com>
2026-05-25 20:45:03 +0530

    lesson 4 activity 3 finished

---
+++ Lessons/lesson 4/activity 3.py
@@ -0,0 +1,14 @@
+print(" Enter the marks obtained in four subjects ")
+
+maths = int(input("maths "))
+english = int(input("english "))
+science = int(input("science "))
+hindi = int(input("hindi "))
+
+sum = maths+english+science+hindi
+print("sum",sum)
+
+percentage = (sum/400)*100
+
+print(end = "Percentage mark = ")
+print(percentage)
