64faf5d970c7a59ce85b1df0916fef5e3057c417
Jesslynn marietta.B <priscillabritto554@gmail.com>
2026-05-29 15:59:11 +0530

    finished project 7

---
+++ Projects/project 7.py
@@ -0,0 +1,12 @@
+# A PROGRAM TO CHECK THE TEMPERATURE OF WEATHER
+
+temperature= float(input("Enter the approximate temperature : "))
+
+if temperature > 5:
+    print(" It is suitable to wear light and soft clothes")
+
+else:
+    print(" Wear thick and warm clothes")
+    
+
+
