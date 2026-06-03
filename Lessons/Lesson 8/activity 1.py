46cf4e3ccc9975c666d081a3027b2dde8799b847
Jesslynn marietta.B <priscillabritto554@gmail.com>
2026-06-03 17:44:48 +0530

    finished lesson 8 activity 1

---
+++ Lessons/Lesson 8/activity 1.py
@@ -0,0 +1,25 @@
+# ACTIVITY 1 - SUM OF N NATURAL NUMBERS
+
+# 1) Ask the user to enter a number and store it in `n`.
+
+# 2) Set `sum` to 0.
+
+# (This will store the running total.)
+
+# 3) Use a `for` loop from 1 to `n` (inclusive):
+
+# - In each step, add the current value of `i` to `sum`.
+
+# 4) After adding, print the current value of `sum`.
+
+# (So the user can see how the sum increases step by step.)
+
+number = int(input(" Enter a number- "))
+sum = 0 
+
+for i in range(1,number+1):
+   print ("i",i)
+   sum = i+sum
+   print (sum)
+
+print(f"sum of {number} natural numbers = {sum}")
